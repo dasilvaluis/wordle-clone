887ef0d5d680f642a8e2a535afb982c76660bf8b
dasilvaluis <15066801+dasilvaluis@users.noreply.github.com>
2022-04-29 11:42:51 +0300

    Refactor clean punctioation function

--- scripts/helpers/text.py
+++ scripts/helpers/text.py
@@ -4,20 +4,23 @@ from functools import reduce
 def text_to_list(text: str):
     return list(text.split(" "))
 
-def clean_punctuation(text: str):
-    punc = '''!()„…’[]{};:'"\,<>./?@#$%^&*_~'''
-    
-    for ele in text:
-        if ele in punc:
-            text = text.replace(ele, "")
-
-    return text
-
-def replace_text_with_pairs(pairs, text):
+def replace_text_with_pairs(pairs: str | List[str], text: str):
     def replace_text(acc, pair):
         return acc.replace(pair[0], pair[1])
 
     return reduce(replace_text, pairs, text)
+
+def standard_pairs_char_to_word(chars: str, clean_word: str):
+    def build_tuple_list(acc, char):
+        return acc + [(char, clean_word)]
+    
+    return reduce(build_tuple_list, chars, [])
+
+def clean_punctuation(text: str):
+    puncts = '''!()„…’[]{};:'"\,<>./?@#$%^&*_~'''
+    pairs = standard_pairs_char_to_word(puncts, '')
+
+    return replace_text_with_pairs(pairs, text)
 
 def clean_joint_letters(text: str):
     pairs = [
@@ -52,17 +55,20 @@ def words_to_lowercase(wordsList: List[str]):
     return map(lambda x : x.lower(), wordsList)
 
 def filter_words_with_minus(wordsList: List[str]):
-    without_minus = lambda x : '-' not in x
+    def without_minus(x):
+        return '-' not in x
 
     return filter(without_minus, wordsList)
 
 def filter_foreign_words(wordsList: List[str]):
     foreign_letters = 'kwyç'
-    probably_not_foreign = lambda w : not any(l in w for l in foreign_letters)
+    def probably_not_foreign(w):
+        return not any(l in w for l in foreign_letters)
 
     return filter(probably_not_foreign, wordsList)
 
 def filter_numbers(wordsList: List[str]):
-    without_numbers = lambda s : not any(i.isdigit() for i in s)
+    def without_numbers(s):
+        return not any(i.isdigit() for i in s)
 
     return filter(without_numbers, wordsList)
