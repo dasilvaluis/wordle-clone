fa35f5962cc4757884997871a489273fa380c320
dasilvaluis <15066801+dasilvaluis@users.noreply.github.com>
2022-06-06 13:50:15 +0300

    Move pdf to text parser to helper file

--- scripts/helpers/files.py
+++ scripts/helpers/files.py
@@ -1,5 +1,10 @@
 import json
 import os
+import io
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
 
 def is_pdf_file(x) :
   return x.split(".")[-1] == "pdf"
@@ -19,3 +24,26 @@ def save_output_to_json(filename, data):
 
     with open(filename, "w", encoding = "utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+
+#converts pdf, returns its text content as a string
+def convert_pdf_to_text(fname, pages = None):
+    pagenums = set() if not pages else set(pages)
+    output = io.StringIO()
+
+    pdf_manager = PDFResourceManager()
+    text_converter = TextConverter(pdf_manager, output, laparams = LAParams())
+    pdf_page_interpreter = PDFPageInterpreter(pdf_manager, text_converter)
+
+    pdf_file = open(fname, 'rb')
+    pages = PDFPage.get_pages(pdf_file, pagenums)
+
+    for page in pages:
+        pdf_page_interpreter.process_page(page)
+    
+    text = output.getvalue()
+
+    output.close()
+    pdf_file.close()
+    text_converter.close()
+
+    return text 
--- scripts/pdf-parse.py
+++ scripts/pdf-parse.py
@@ -1,10 +1,4 @@
-import io
 import os
-import json
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
 
 from helpers.text import (
     clean_duplicates,
@@ -26,33 +20,10 @@ from helpers.functional import (
     tap_print,
 )
 
-from helpers.files import find_pdf_files, save_output_to_json
+from helpers.files import convert_pdf_to_text, find_pdf_files, save_output_to_json
 from scripts.helpers.utils import find_duplicated_words
 
 NEEDED_WORD_LENGTH = 5
-
-#converts pdf, returns its text content as a string
-def convert_pdf_to_text(fname, pages = None):
-    pagenums = set() if not pages else set(pages)
-    output = io.StringIO()
-
-    pdf_manager = PDFResourceManager()
-    text_converter = TextConverter(pdf_manager, output, laparams = LAParams())
-    pdf_page_interpreter = PDFPageInterpreter(pdf_manager, text_converter)
-
-    pdf_file = open(fname, 'rb')
-    pages = PDFPage.get_pages(pdf_file, pagenums)
-
-    for page in pages:
-        pdf_page_interpreter.process_page(page)
-    
-    text = output.getvalue()
-
-    output.close()
-    pdf_file.close()
-    text_converter.close()
-
-    return text 
 
 cleanup_words_list = composite(
     clean_duplicates,
